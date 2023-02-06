578d2a9ddc42c0fd5968e9925c0edcf09a5d72b8
Filipe Lander <filipelander@tutanota.de>
2023-02-06 13:20:12 -0300

    font-weight

--- src/pages/SobreMim.jsx
+++ src/pages/SobreMim.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const SobreMim = () => {
   return (
     <div className='sobremim-container'>
-      <h1 className='font-thin pt-8 pb-6 text-4xl text-slate-700 text-center'>
-        Sobre mim Projetos
+      <h1 className='font-thin pt-10 pb-4 text-4xl text-slate-800 text-center'>
+        Sobre mim
       </h1>
 
-      <p className='mt-4 px-4 font-normal text-sm text-slate-500  '>
+      <p className='mt-4 px-4 font-light text-sm text-slate-600  '>
         Tenho 34 anos e comecei meus estudos em front-end em janeiro de 2022.
         Conheço HTML e CSS e neste momento estou iniciando meus estudos em
         javascript. Sei trabalhar com ferramentas de versionamento de código

fc84240d294c49c601798da5e465cfc2cf971abe
Filipe Lander <96532223+filipelanderst@users.noreply.github.com>
2023-06-23 09:20:30 -0300

    Update LinkedInGithub.jsx

--- src/components/LinkedInGithub.jsx
+++ src/components/LinkedInGithub.jsx
@@ -5,7 +5,7 @@ import LinkedIn from '../icons/linkedIn.svg';
 const LinkedInGithub = () => {
   return (
     <div className='anima-bottom mdlg:grid mdlg:grid-cols-2 gap-4 w-6 mdlg:w-16 absolute left-0 top-0 ml-[10vw] mdlg:ml-[50px] pt-[75vh] xl:pt-[85vh]'>
-      <a href='https://github.com/flx-lander7' target='_blank' translate='no'>
+      <a href='https://github.com/filipelanderst' target='_blank' translate='no'>
         <img src={Github} alt='github icon' className='ml-4 py-2' />
       </a>
 
